99cc419c48637a6f92cf136b3d3f16c1fba9eef3
ZgjimDida <zgjimdida55@hotmail.com>
2021-11-30 23:34:54 +0100

    Add some style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,19 +10,24 @@ import { IInflationRate } from './inflation-rate.model';
 export class AppComponent {
   inflationRate: IInflationRate[] = [];
   salary: number = 0;
+  years: number[] = [];
+  selectedYear: number = 2003;
 
   ngOnInit() {
     this.inflationRate = INFLATION_RATE;
-    console.log(this.inflationRate);
+    this.years = this.inflationRate.map(item => item.year);
   }
 
   calculateInflation(): void {
     let salary = this.salary;
     let salary2 = this.salary;
+    this.inflationRate = INFLATION_RATE.filter(item => item.year >= this.selectedYear);
+
     this.inflationRate.forEach(item => {
       salary = salary + (salary / 100 * item.percentage * -1);
       item.salaryBasedOnInflation = salary;
     })
+
     this.inflationRate.forEach(item => {
       salary2 = salary2 + (salary2 / 100 * item.percentage);
       item.salaryToHave = salary2;
